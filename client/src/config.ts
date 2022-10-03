be7a9c43f706d35ea665a6cfa51e5ebe956e4ba8
Munene Brian <52892865+Munene-brian@users.noreply.github.com>
2022-10-04 02:24:05 +0300

    init

---
+++ client/src/config.ts
@@ -0,0 +1,9 @@
+const apiId = 'egcpmdaty9';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+  // TODO: Create an Auth0 application and copy values from it into this map. For example:
+  // domain: 'dev-nd9990-p4.us.auth0.com',
+export const authConfig = {
+  domain: 'dev-69uokptf.us.auth0.com',
+  clientId: 'XhsLwsfaw7QIjA26jHbEDlh6AMsdLZH2',
+  callbackUrl: 'http://localhost:3000/callback'
+};
